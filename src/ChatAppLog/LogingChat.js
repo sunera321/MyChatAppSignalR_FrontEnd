d49afb3ebbec207fa91babde2d7b16516f9bac17
Sunera Sudaraka Konara <110524387+sunera321@users.noreply.github.com>
2024-09-07 23:45:14 +0530

    Update UI in LogingChat.js and WaitingRoom.js

--- src/ChatAppLog/LogingChat.js
+++ src/ChatAppLog/LogingChat.js
@@ -1,45 +1,38 @@
 import React from 'react'
 
 const LogingChat = () => {
-    return (
-        <div className="relative min-h-screen overflow-hidden text-white bg-gray-900">
-          {/* Background decorations */}
-          <div className="absolute w-64 h-64 bg-purple-500 rounded-full top-20 left-20 mix-blend-multiply filter blur-xl opacity-70 animate-pulse"></div>
-          <div className="absolute w-64 h-64 bg-blue-500 rounded-full top-40 right-20 mix-blend-multiply filter blur-xl opacity-70 animate-pulse"></div>
-    
-          {/* Header */}
-          <header className="flex items-center justify-between p-4">
-            <div className="text-2xl font-bold">ChatChat.gg</div>
-            {/* <nav className="hidden md:block">
-              <a href="#" className="mx-2 hover:text-purple-400">Home</a>
-              <a href="#" className="mx-2 hover:text-purple-400">Blog</a>
-              <a href="#" className="mx-2 hover:text-purple-400">About</a>
-              <a href="#" className="mx-2 hover:text-purple-400">Contact</a>
-            </nav>
-          <button className="px-4 py-2 transition bg-purple-600 rounded hover:bg-purple-700">Login</button> */}
-          </header>
-    
-          {/* Main Content */}
-          <main className="container px-4 mx-auto mt-16">
-            <div className="flex flex-col items-center md:flex-row">
-              {/* Left Column */}
-              <div className="mb-8 md:mb-0">
-                <h1 className="mb-4 text-4xl font-bold md:text-5xl">Talk to strangers, Make friends!</h1>
-                <p className="mb-8 text-xl">
-                
-Welcome to the ultimate social chat experience! Our app revolutionizes the way you connect online.
-With users from every time zone, there is always someone online ready to chat. Say goodbye to lonely nights and hello to round-the-clock companionship.
-                </p>
-                <button className="px-6 py-3 text-lg font-semibold transition bg-blue-600 rounded-full hover:bg-blue-700">
-                  <a href="/chathome">Start Chatting</a>
-                </button>
-              </div>
-    
-            </div>
-          </main>
+  return (
+    <div className="relative min-h-screen overflow-hidden text-white bg-gray-900">
+      {/* Background decorations */}
+      <div className="absolute w-64 h-64 bg-purple-500 rounded-full top-20 left-20 mix-blend-multiply filter blur-xl opacity-70 animate-pulse"></div>
+      <div className="absolute w-64 h-64 bg-blue-500 rounded-full top-40 right-20 mix-blend-multiply filter blur-xl opacity-70 animate-pulse"></div>
+
+      {/* Header */}
+      <header className="flex items-center justify-between p-4">
+        <div className="text-2xl font-bold">ChatChat.gg</div>
+             </header>
+
+      {/* Main Content */}
+      <main className="container px-4 mx-auto mt-16">
+        <div className="flex flex-col items-center md:flex-row">
+          {/* Left Column */}
+          <div className="mb-8 md:mb-0">
+            <h1 className="mb-4 text-4xl font-bold md:text-5xl">Talk to strangers, Make friends!</h1>
+            <p className="mb-8 text-xl">
+
+              Welcome to the ultimate social chat experience! Our app revolutionizes the way you connect online.
+              With users from every time zone, there is always someone online ready to chat. Say goodbye to lonely nights and hello to round-the-clock companionship.
+            </p>
+            <button className="px-6 py-3 text-lg font-semibold transition bg-blue-600 rounded-full hover:bg-blue-700">
+              <a href="/chathome">Start Chatting</a>
+            </button>
+          </div>
+
         </div>
-      );
-    
+      </main>
+    </div>
+  );
+
 }
 
 export default LogingChat
--- src/Components/WaitingRoom.js
+++ src/Components/WaitingRoom.js
@@ -1,12 +1,33 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+const predefinedChatRooms = [
+    'game',
+    'music',
+    'songs',
+    'education',
+    'tech',
+    'news'
+];
+
 const WaitingRoom = ({ JoinChatRoom }) => {
-    const [UserName, setUserName] = useState('');
-    const [ChatRoom, setChatRoom] = useState('');
+    const [userName, setUserName] = useState('');
+    const [chatRoom, setChatRoom] = useState('');
+    const [isCustomChatRoom, setIsCustomChatRoom] = useState(true);
 
     const handleJoin = () => {
-        JoinChatRoom(UserName, ChatRoom);
+        JoinChatRoom(userName, chatRoom);
+    };
+
+    const handleChatRoomChange = (e) => {
+        const selectedChatRoom = e.target.value;
+        if (selectedChatRoom === 'custom') {
+            setIsCustomChatRoom(true);
+            setChatRoom('');
+        } else {
+            setIsCustomChatRoom(false);
+            setChatRoom(selectedChatRoom);
+        }
     };
 
     return (
@@ -19,22 +40,36 @@ const WaitingRoom = ({ JoinChatRoom }) => {
                     <input
                         type="text"
                         className="w-full p-2 border rounded border-cyan-950"
-                        value={UserName}
+                        value={userName}
                         required
                         onChange={(e) => setUserName(e.target.value)}
                         placeholder="Enter your name"
                     />
                 </div>
                 <div className="mb-4">
-                    <label className="block mb-2 text-gray-700 ">Chat Room</label>
-                    <input
-                        type="text"
+                    <label className="block mb-2 text-gray-700">Chat Room</label>
+                    <select
                         className="w-full p-2 border rounded border-cyan-950"
-                        value={ChatRoom}
-                        required
-                        onChange={(e) => setChatRoom(e.target.value)}
-                        placeholder="Enter chat room name"
-                    />
+                        onChange={handleChatRoomChange}
+                        value={isCustomChatRoom ? 'custom' : chatRoom}
+                    >
+                        <option value="custom">Create New Chat Room</option>
+                        {predefinedChatRooms.map(room => (
+                            <option key={room} value={room}>
+                                {room}
+                            </option>
+                        ))}
+                    </select>
+                    {isCustomChatRoom && (
+                        <input
+                            type="text"
+                            className="w-full p-2 mt-2 border rounded border-cyan-950"
+                            value={chatRoom}
+                            required
+                            onChange={(e) => setChatRoom(e.target.value)}
+                            placeholder="Enter chat room name"
+                        />
+                    )}
                 </div>
                 <button
                     className="w-full p-2 text-white bg-blue-600 rounded hover:bg-blue-700"
